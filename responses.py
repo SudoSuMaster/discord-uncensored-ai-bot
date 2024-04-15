334d94bf56ddd1793037eaf6c2a8374316cb6790
Sudo Su Master <75373825+SudoSuMaster@users.noreply.github.com>
2024-04-15 17:19:39 +0100

    Update responses.py

--- responses.py
+++ responses.py
@@ -35,8 +35,6 @@ def get_response(user_input: str) -> str:
         return 'Good, thanks!'
     elif 'bye' in lowered:
         return 'See you!'
-    elif 'What color is angel' in lowered:
-        return 'black'
     elif 'roll dice' in lowered:
         return f'You rolled: {randint(1, 6)}'
     elif '!ask' in lowered:
